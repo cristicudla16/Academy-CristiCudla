7edb19856a7379c0990ff86182d79c63eabcf2f1
Cristi Cudla <123880859+cristicudla16@users.noreply.github.com>
2026-03-20 16:23:55 +0200

    Updateting the files in LWC

--- Challange8/Challange8/force-app/main/default/lwc/accountWeather/accountWeather.js
+++ Challange8/Challange8/force-app/main/default/lwc/accountWeather/accountWeather.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+<template>
+    <lightning-card title="Account Weather" icon-name="utility:world">
+        <div class="slds-p-around_medium">
+            <template lwc:if={weatherData}>
+                <div class="slds-grid slds-grid_vertical-align-center">
+                    <img src={iconUrl} alt="Weather Icon" />
+                    <div class="slds-m-left_medium">
+                        <p class="slds-text-heading_small">Description {weatherDescription}</p>
+                        <p class="slds-text-heading_small">Temperature {weatherTemp}</p>
+                    </div>
+                </div>
+            </template>
 
-export default class AccountWeather extends LightningElement {}
+            <template lwc:else if:true={error}>
+                <p class="slds-text-color_error">{error}</p>
+            </template>
+        </div>
+    </lightning-card>
+</template>

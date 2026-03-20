7e3bd2152f062b6feb3a53fb55776cc08f9ce5cb
Cristi Cudla <123880859+cristicudla16@users.noreply.github.com>
2026-03-20 16:39:50 +0200

    Updateting files to deploy to org
    Updateting the files to deploy to org and try to get 100%test rate

--- Challange8/Challange8/force-app/main/default/lwc/accountWeather/accountWeather.js
+++ Challange8/Challange8/force-app/main/default/lwc/accountWeather/accountWeather.js
@@ -1,19 +1,52 @@
-<template>
-    <lightning-card title="Account Weather" icon-name="utility:world">
-        <div class="slds-p-around_medium">
-            <template lwc:if={weatherData}>
-                <div class="slds-grid slds-grid_vertical-align-center">
-                    <img src={iconUrl} alt="Weather Icon" />
-                    <div class="slds-m-left_medium">
-                        <p class="slds-text-heading_small">Description {weatherDescription}</p>
-                        <p class="slds-text-heading_small">Temperature {weatherTemp}</p>
-                    </div>
-                </div>
-            </template>
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import CITY_FIELD from '@salesforce/schema/Account.BillingCity';
+import getWeatherData from '@salesforce/apex/WeatherService.getWeatherData';
 
-            <template lwc:else if:true={error}>
-                <p class="slds-text-color_error">{error}</p>
-            </template>
-        </div>
-    </lightning-card>
-</template>
+export default class AccountWeather extends LightningElement {
+    @api recordId;
+    weatherData;
+    error;
+
+    @wire(getRecord, { recordId: '$recordId', fields: [CITY_FIELD] })
+    wiredAccount({ error, data }) {
+        if (data) {
+            const city = getFieldValue(data, CITY_FIELD);
+            if (city) {
+                this.handleFetchWeather(city);
+            } else {
+                this.error = 'Failed to retrieve account city';
+            }
+        } else if (error) {
+            this.error = 'Failed to retrieve account city';
+        }
+    }
+
+    async handleFetchWeather(city) {
+        try {
+            const data = await getWeatherData({ cityName: city });
+            if (data && data.main && data.weather && data.weather.length > 0) {
+                this.weatherData = data;
+                this.error = undefined;
+            } else {
+                this.error = 'Invalid weather data structure returned by the API.';
+            }
+        } catch (err) {
+            this.error = 'Failed to retrieve weather data: ' + (err.body?.message || err.message);
+            this.weatherData = undefined;
+        }
+    }
+
+    get weatherDescription() {
+        return this.weatherData?.weather[0]?.description;
+    }
+
+    get weatherTemp() {
+        return this.weatherData?.main?.temp;
+    }
+
+    get iconUrl() {
+        const icon = this.weatherData?.weather[0]?.icon;
+        return icon ? `https://openweathermap.org/img/wn/${icon}@2x.png` : '';
+    }
+}
